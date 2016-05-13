008ab6f7d1eeafdd5c35c578904086958e78e28d
MDoan66 <Michael.Doan@ihs.com>
2016-05-13 09:51:08 -0500

    TKS_Installer Initial
    Create projects
    create branch

---
+++ MD/TKS_Installer/TKS_Installer/MD.TKS_Installer.UI.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MD.TKS_Installer.UI.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
