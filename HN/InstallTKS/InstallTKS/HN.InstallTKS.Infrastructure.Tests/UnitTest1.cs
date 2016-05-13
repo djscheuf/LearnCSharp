42dabcd97d45056e70318ba43ebf3845de9065e6
HoangYen <hoangyen.nguyen@ihs.com>
2016-05-13 09:38:06 -0500

    TKS install, initial
    Create Branch
    Create project

---
+++ HN/InstallTKS/InstallTKS/HN.InstallTKS.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HN.InstallTKS.Infrastructure.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
