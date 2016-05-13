7d5c0c8f1463bb7943f5d74ab29393184aa9e226
MDoan66 <Michael.Doan@ihs.com>
2016-05-13 09:51:08 -0500

    TKS_Installer Initial
    Create projects
    create branch

---
+++ MD/TKS_Installer/TKS_Installer/MD.TKS_Installer.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.TKS_Installer.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
